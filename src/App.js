8bbc5f925d0537f9b3b046eae211c2f488b2200f
Hnatkiv <gnatkiv@icloud.com>
2023-01-17 18:15:10 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import axios from "axios";
+import Movie from "./Movie";
+import './app.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+
+class App extends Component {
+
+  state = {
+    isLoading: true,
+    movies: []
+  }
+
+  getMovies = async () => {
+    const {data: {data: {movies}}} = await axios.get('https://yts.mx/api/v2/list_movies.json?sort_by=raiting');
+    this.setState({movies: movies, isLoading: false})
+  }
+
+  componentDidMount() {
+    this.getMovies();
+  }
+
+  render() {
+    const {isLoading, movies} = this.state;
+
+    return (
+      <section className="container">
+        {isLoading ? (
+          <div className="loader">
+            <span className="loader__text">Downloading.......</span>
+          </div>
+        ) : (
+          <div className="movies">
+            {movies.map(movie => (
+              <Movie
+                  key={movie.id}
+                  id={movie.id} 
+                  summary={movie.summary} 
+                  poster={movie.medium_cover_image}
+                  year={movie.year}
+                  title={movie.title}
+                  genres={movie.genres}/>
+            ))}
+          </div>
+        )}
+      </section>
+
+    );
+  }
 }
 
 export default App;
---
+++ src/Movie.js
@@ -0,0 +1,34 @@
+import React from "react";
+import PropTipes from "prop-types";
+import './movie.css';
+
+
+function Movie({id, title, summary, poster, year, genres}) {
+    return (
+        <div className="movie">
+            <div className="movie__column">
+                <h3 className="movie__title">{title}</h3>
+                <h5 className="movie__year">{year}</h5>
+                <p className="movie__summary">{summary.slice(0, 140)}...</p>
+                <ul className="movie__genres">
+                    {genres.map((genre, i) => {
+                        return <li key={i} className="genres__genre">{genre}</li>
+                    })}
+                </ul>
+            </div>
+            <img src={poster} alt={title} />
+
+        </div>
+    )
+}
+
+Movie.propTipes = {
+    id: PropTipes.number.isRequired,
+    title: PropTipes.string.isRequired,
+    summary: PropTipes.string.isRequired,
+    poster: PropTipes.string.isRequired,
+    year: PropTipes.number.isRequired,
+    genres: PropTipes.arrayOf(PropTipes.string).isRequired
+}
+
+export default Movie;
